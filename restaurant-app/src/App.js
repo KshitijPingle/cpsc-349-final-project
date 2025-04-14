1fb4af2016261cb0d0ef2c26650ed48a3af6503b
kalmk <111930167+kalmk@users.noreply.github.com>
2025-04-14 13:25:57 -0700

    set up for header and footer

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -1,9 +1,12 @@
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      {/* stuff inside */}
+      <Footer />
     </div>
   );
 }
