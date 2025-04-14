86afc0e8f05973b94e391fafd9d0275f615a097f
kalmk <111930167+kalmk@users.noreply.github.com>
2025-04-14 13:22:51 -0700

    header

--- restaurant-app/src/components/Header.js
+++ restaurant-app/src/components/Header.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from "react";
 
+import "../css/Header.css";
+
+// TODO: Header.js
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  return <div>Header</div>;
+};
