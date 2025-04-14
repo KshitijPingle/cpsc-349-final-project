a6ef8d5a653f5217ba9ef4a77681938c0c48c1a9
kalmk <111930167+kalmk@users.noreply.github.com>
2025-04-14 13:27:15 -0700

    Update Footer.js

--- restaurant-app/src/components/Footer.js
+++ restaurant-app/src/components/Footer.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
+
+import "../css/Footer.css";
 
 export const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  return <div>Footer</div>;
+};
