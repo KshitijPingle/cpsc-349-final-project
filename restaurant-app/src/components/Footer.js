40f831ff3e193406e0468fb53f92848307050f9c
kalmk <111930167+kalmk@users.noreply.github.com>
2025-04-20 11:31:57 -0700

    lets use styled componenets for simplicity

--- restaurant-app/src/components/Footer.js
+++ restaurant-app/src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "../css/Footer.css";
-
 // TODO Footer
 export const Footer = () => {
   return <div>Footer</div>;
--- restaurant-app/src/components/Header.js
+++ restaurant-app/src/components/Header.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "../css/Header.css";
-
 // TODO: Header.js
 export const Header = () => {
   return <div>Header</div>;
