b05098c42e56e4076da0aec4ac012868607f2a60
kalmk <111930167+kalmk@users.noreply.github.com>
2025-04-19 20:30:55 -0700

    set up routing

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -2,6 +2,8 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import { Menu } from "./pages/Menu";
+import { Home } from "./pages/Home"
+import { CheckOut } from "./pages/Checkout"
 
 function App() {
   return (
@@ -9,9 +11,13 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path="/menu" element={<Menu />} />
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="checkout" element={<CheckOut />} />
+        </Route>
       </Routes>
-      
+
       <Footer />
     </div>
   );
--- restaurant-app/src/pages/Menu.js
+++ restaurant-app/src/pages/Menu.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Menu = () => {
   return (
-    <div>hii</div>
+    <div>Menu</div>
   )
 }
