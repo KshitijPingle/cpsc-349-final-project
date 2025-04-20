51178cfc93a7c2e7e18365066ea09202806531a4
kalmk <111930167+kalmk@users.noreply.github.com>
2025-04-20 11:50:47 -0700

    working on Home page. Added styled componenets. Thinking about using CSS frameworks or preprocessors

--- restaurant-app/src/pages/Home.js
+++ restaurant-app/src/pages/Home.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Burger from "../img/greetingBurger.jpg";
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  color: #333;
+  text-align: center;
+`;
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+  max-width: 100%;
+  height: auto;
+`;
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Wrapper>
+      <Title>Welcome to Joe's Burger</Title>
+      <Image src={Burger} />
+    </Wrapper>
+  );
+};
