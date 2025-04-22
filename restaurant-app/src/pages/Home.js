4c186176132474c73ac9678a6e0a7ebad419d4ae
robinwinter1 <111930167+kalmk@users.noreply.github.com>
2025-04-21 18:17:01 -0700

    separated the deals in home into a componenet

--- restaurant-app/src/pages/Home.js
+++ restaurant-app/src/pages/Home.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Burger from "../img/greetingBurger.jpg";
-import Deal1 from "../img/doubleBurger.jpg";
-import Deal2 from "../img/chickenNugget.jpg";
-import Deal3 from "../img/newMenu.jpg";
 import { AboutUs } from "../components/AboutUs";
 import { Link } from "react-router-dom";
+import { HomeDeals } from "../components/HomeDeals";
 
 export const Home = () => {
   return (
@@ -36,92 +34,7 @@ export const Home = () => {
       {/* Deals Section */}
       <div className="row" style={{ marginTop: "150px" }}>
         <h1 className="text-dark text-center">Deals and Offers</h1>
-
-        {/* Deal 1 */}
-        <div className="col-md-4 mb-4">
-          <div className="card h-100 shadow-sm rounded">
-            <img
-              src={Deal1}
-              className="card-img-top"
-              alt="Veggie Hamburger"
-              style={{ height: "500px", objectFit: "cover" }}
-            />
-            <div className="card-body d-flex flex-column">
-              <h6 className="text-danger fw-bold text-center">Limited time</h6>
-              <h5 className="card-title fw-bold text-center">
-                2 Classic Veggie Hamburger with Fries
-              </h5>
-              <p className="card-text">
-                Enjoy two delicious veggie hamburgers, perfectly seasoned and
-                paired with crispy fries. Fresh, satisfying, and a fantastic
-                choice for any time of day.
-              </p>
-              <div className="d-flex justify-content-center mt-auto">
-                <Link to="/menu">
-                  <button className="btn btn-danger">View Menu</button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Deal 2 */}
-        <div className="col-md-4 mb-4">
-          <div className="card h-100 shadow-sm rounded">
-            <img
-              src={Deal2}
-              className="card-img-top"
-              alt="Pineapple Dragonfruit"
-              style={{ height: "500px", objectFit: "cover" }}
-            />
-            <div className="card-body d-flex flex-column">
-              <h6 className="text-danger fw-bold text-center">
-                Customer Favorite
-              </h6>
-              <h5 className="card-title fw-bold text-center">
-                Chicken Nuggets with Sauces
-              </h5>
-              <p className="card-text">
-                Golden, crispy chicken nuggets served with a variety of tasty
-                dipping sauces. A delightful snack or meal for anyone craving
-                something savory and flavorful.
-              </p>
-              <div className="d-flex justify-content-center mt-auto">
-                <Link to="/menu">
-                  <button className="btn btn-danger">View Menu</button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Deal 3 */}
-        <div className="col-md-4 mb-4">
-          <div className="card h-100 shadow-sm rounded">
-            <img
-              src={Deal3}
-              className="card-img-top"
-              alt="Pineapple Dragonfruit"
-              style={{ height: "500px", objectFit: "cover" }}
-            />
-            <div className="card-body d-flex flex-column">
-              <h6 className="text-danger fw-bold text-center">New Menu</h6>
-              <h5 className="card-title fw-bold text-center">
-                Premium Double Cheeseburger
-              </h5>
-              <p className="card-text">
-                Indulge in a juicy double cheeseburger, topped with sweet
-                caramelized onions and melted cheese. A flavorful combination
-                that's sure to satisfy your cravings.
-              </p>
-              <div className="d-flex justify-content-center mt-auto">
-                <Link to="/menu">
-                  <button className="btn btn-danger">View Menu</button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
+        <HomeDeals />
 
         {/* About us */}
         <div
