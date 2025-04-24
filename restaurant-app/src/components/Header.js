9d8a7ecb4c61ddd5d45466890f2fa58a539ec786
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2025-04-24 16:51:57 -0700

    WIP header

--- restaurant-app/src/components/Header.js
+++ restaurant-app/src/components/Header.js
@@ -2,5 +2,16 @@ import React from "react";
 
 // TODO: Header.js
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/" style={{marginLeft: '15px'}}>
+        {/* {Insert logo here later} */}
+        <p>Joe's Burger Logo</p>
+      </a>
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" href="/menu/deals">Menu</a>
+        <a class="nav-item nav-link" href="checkout">Checkout</a>
+      </div>
+    </nav>
+);
 };
