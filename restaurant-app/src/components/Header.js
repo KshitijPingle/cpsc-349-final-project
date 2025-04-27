c9a65475bd50101dcdeebf24b2d8e2970f18e9a7
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2025-04-26 20:26:47 -0700

    Updated header and footer

--- restaurant-app/src/components/Header.js
+++ restaurant-app/src/components/Header.js
@@ -3,14 +3,14 @@ import React from "react";
 // TODO: Header.js
 export const Header = () => {
   return (
-    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar sticky-top navbar-expand-lg" style={{backgroundColor:"#dc3545"}}>
       <a class="navbar-brand" href="/" style={{marginLeft: '15px'}}>
         {/* {Insert logo here later} */}
-        <p>Joe's Burger Logo</p>
+        <p style={{color:"white"}}>Joe's Burger Logo</p>
       </a>
       <div class="navbar-nav">
-        <a class="nav-item nav-link" href="/menu/deals">Menu</a>
-        <a class="nav-item nav-link" href="checkout">Checkout</a>
+        <a class="nav-item nav-link" href="/menu/deals" style={{color:"white", marginLeft:"10px"}}>Menu</a>
+        <a class="nav-item nav-link" href="checkout" style={{color:"white", marginLeft:"10px"}}>Checkout</a>
       </div>
     </nav>
 );
