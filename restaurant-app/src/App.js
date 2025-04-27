cf2748aeed5ecc1646ac77bd004bfa67493d1295
robinwinter1 <111930167+kalmk@users.noreply.github.com>
2025-04-26 23:47:39 -0700

    fix

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -2,7 +2,7 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { CheckOut } from "./pages/CheckOut";
+import { Checkout } from "./pages/Checkout";
 import { Burgers } from "./pages/Burgers";
 import { Sides } from "./pages/Sides";
 import { Drinks } from "./pages/Drinks";
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
-          <Route path="checkout" element={<CheckOut />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/menu/deals" element={<Deals />} />
           <Route path="/menu/burgers" element={<Burgers />} />
           <Route path="/menu/sides" element={<Sides />} />
--- restaurant-app/src/pages/Checkout.js
+++ restaurant-app/src/pages/Checkout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const CheckOut = () => {
+export const Checkout = () => {
   return (
-    <div>CheckOut</div>
+    <div>Checkout</div>
   )
 }
