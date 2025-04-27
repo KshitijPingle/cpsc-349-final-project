79e22213313d556ea4765243653f7ced439518be
robinwinter1 <111930167+kalmk@users.noreply.github.com>
2025-04-26 21:11:54 -0700

    decided to use only 1 deal

--- restaurant-app/src/pages/Deals.js
+++ restaurant-app/src/pages/Deals.js
@@ -35,56 +35,6 @@ export const Deals = () => {
               </div>
             </div>
           </div>
-          {/* Deal 2 */}
-          <div className="col-md-4 mb-4">
-            <div className="card h-100 shadow-sm rounded">
-              <img
-                src={Deal1}
-                className="card-img-top"
-                alt="Veggie Hamburger"
-                style={{ height: "500px", objectFit: "cover" }}
-              />
-              <div className="card-body d-flex flex-column">
-                <h6 className="text-danger fw-bold text-center">
-                  Limited time
-                </h6>
-                <h5 className="card-title fw-bold text-center">
-                  2 Classic Veggie Hamburger with Fries
-                </h5>
-                <p className="card-text text-center">$18.99</p>
-                <div className="d-flex justify-content-center mt-auto">
-                  <Link to="/menu">
-                    <button className="btn btn-danger">Add to cart</button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Deal 3 */}
-          <div className="col-md-4 mb-4">
-            <div className="card h-100 shadow-sm rounded">
-              <img
-                src={Deal1}
-                className="card-img-top"
-                alt="Veggie Hamburger"
-                style={{ height: "500px", objectFit: "cover" }}
-              />
-              <div className="card-body d-flex flex-column">
-                <h6 className="text-danger fw-bold text-center">
-                  Limited time
-                </h6>
-                <h5 className="card-title fw-bold text-center">
-                  2 Classic Veggie Hamburger with Fries
-                </h5>
-                <p className="card-text text-center">$18.99</p>
-                <div className="d-flex justify-content-center mt-auto">
-                  <Link to="/menu">
-                    <button className="btn btn-danger">Add to cart</button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
