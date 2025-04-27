8a5f74117f79c94d8822e4abd923d61f42cb466f
robinwinter1 <111930167+kalmk@users.noreply.github.com>
2025-04-26 20:46:58 -0700

    fixed linting error due to href link and changed it to button.

--- restaurant-app/src/components/Footer.js
+++ restaurant-app/src/components/Footer.js
@@ -1,57 +1,71 @@
 import React from "react";
 
-// TODO Footer
 export const Footer = () => {
   return (
-  <footer class="row row-cols-1 row-cols-md-5 py-5 my-5 border-top">
-    <div class="col md-3">
-      <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
-        {/* Insert Joe's Burger Logo Here */}
-        <p>Logo</p>
-      </a>
-    </div>
-    <div class="col mb-3">
-      <h5>Location</h5>
-      <div class="nav flex-column">
-        <div class="nav-tem mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Address: ABCD</a>
-        </div>
-        <div class="nav-tem mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Phone: XXX-XXX-XXXX</a>
-        </div>
-        <div class="nav-tem mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Open hours: 7am - 11pm</a>
-        </div>
-        <div class="nav-tem mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">FAQ</a>
+    <footer className="row row-cols-1 row-cols-md-5 py-5 my-5 border-top">
+      <div class="col md-3">
+        <a
+          href="/"
+          class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none"
+        >
+          {/* Insert Joe's Burger Logo Here */}
+          <p>Logo</p>
+        </a>
+      </div>
+
+      <div className="col mb-3">
+        <h5>Location</h5>
+        <div className="nav flex-column">
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Address: ABCD
+          </button>
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Phone: XXX-XXX-XXXX
+          </button>
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Open hours: 7am - 11pm
+          </button>
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            FAQ
+          </button>
         </div>
       </div>
-    </div>
-    <div class="col mb-3">
-      <h5>Legal</h5>
-      <div class="nav flex-column">
-        <div class="nav-tem mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Terms and Conditions</a>
-        </div>
-        <div class="nav-tem mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Do Not Sell My Personal Information</a>
-        </div>
-        <div class="nav-tem mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Cookie Preferences</a>
-        </div>
-        <div class="nav-tem mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Privacy Policies</a>
+
+      <div className="col mb-3">
+        <h5>Legal</h5>
+        <div className="nav flex-column">
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Terms and Conditions
+          </button>
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Do Not Sell My Personal Information
+          </button>
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Cookie Preferences
+          </button>
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Privacy Policies
+          </button>
         </div>
       </div>
-    </div>
-    <div class="col mb-3">
-      <h5>Join our Community</h5>
-      <a href="#" class="nav-link p-0 text-body-secondary">Rewards</a>
-      <a href="#" class="nav-link p-0 text-body-secondary">Jobs</a>
-      <a href="#" class="nav-link p-0 text-body-secondary">Franchising</a>
-      <a href="#" class="nav-link p-0 text-body-secondary">Investors</a>
-    </div>
-    <div class="col mb-3"></div>
-  </footer>
+
+      <div className="col mb-3">
+        <h5>Join our Community</h5>
+        <div className="nav flex-column">
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Rewards
+          </button>
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Jobs
+          </button>
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start mb-2">
+            Franchising
+          </button>
+          <button className="nav-link p-0 text-body-secondary bg-transparent border-0 text-start">
+            Investors
+          </button>
+        </div>
+      </div>
+    </footer>
   );
 };
