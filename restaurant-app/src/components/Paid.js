06c452e58bf9bbb700dd244aacb855a841c7e5d2
robinwinter1 <111930167+kalmk@users.noreply.github.com>
2025-04-27 15:14:57 -0700

    changed UI for Paid

--- restaurant-app/src/components/Paid.js
+++ restaurant-app/src/components/Paid.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Paid = () => {
   return (
-    <div>
-        Successfully paid! Thank you for your purchase.
-        Return to Menu
+    <div className="d-flex vh-100 justify-content-center align-items-center text-center">
+      <div>
+        <h2 className="mb-4">
+          Successfully paid! Thank you for your purchase.
+        </h2>
+        <Link to="/" className="btn btn-danger">
+          Return to Menu
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
