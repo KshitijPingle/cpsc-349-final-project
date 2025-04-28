408ea0cf471b6e6638cfee0a2b6c5eb60d14420a
robinwinter1 <111930167+kalmk@users.noreply.github.com>
2025-04-28 14:15:12 -0700

    removed Link due to navigate

--- restaurant-app/src/pages/Deals.js
+++ restaurant-app/src/pages/Deals.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Menu } from "../components/Menu";
 import Deal1 from "../img/doubleBurger.jpg";
-import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 
@@ -38,19 +37,17 @@ export const Deals = () => {
                 </h5>
                 <p className="card-text text-center">$18.99</p>
                 <div className="d-flex justify-content-center mt-auto">
-                  <Link to="/checkout">
-                    <button
-                      className="btn btn-danger"
-                      onClick={() =>
-                        handleAddToCart({
-                          name: "2 Classic Veggie Hamburger with Fries",
-                          price: 18.99,
-                        })
-                      }
-                    >
-                      Add to cart
-                    </button>
-                  </Link>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() =>
+                      handleAddToCart({
+                        name: "2 Classic Veggie Hamburger with Fries",
+                        price: 18.99,
+                      })
+                    }
+                  >
+                    Add to cart
+                  </button>
                 </div>
               </div>
             </div>
